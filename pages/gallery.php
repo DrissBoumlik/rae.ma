3a34f6401ae7095afd4a97a8dafb1f03a810323e
Driss Boumlik <boumlik.t@gmail.com>
2019-02-24 17:37:03 +0100

    translation

--- pages/gallery.php
+++ pages/gallery.php
@@ -15,85 +15,9 @@
 <div class="gallery">
     <div class="">
         <div class="row">
-            <!-- <div class="col-md-4">
-                <div class="youtube">
-                    <div class="ukraine">
-                        <h2>Ukraine</h2>
-                        <div class="row"><iframe src="https://www.youtube.com/embed/XpTAlgdgfDk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
-                        <div class="row"><iframe src="https://www.youtube.com/embed/fBGHdqXuD0E" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
-                        <div class="row"><iframe src="https://www.youtube.com/embed/v=nGU2PiaoMfA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
-                    </div>
-                    <div class="russia">
-                        <h2>Russia</h2>
-                        <div class="row"><iframe src="https://www.youtube.com/embed/rVuAf-0RpTA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
-                        <div class="row"><iframe src="https://www.youtube.com/embed/iChZF9MVB4g" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
-                        <div class="row"><iframe src="https://www.youtube.com/embed/WD54QDyRwoQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-8">
-                <div class="gallery-container">
-
-                    <h1><?php echo (($lang == 'ar') ? 'رواق' : (($lang == 'en') ? 'Gallery' : 'Galerie')); ?></h1>
-
-                    <p class="page-description text-center">Grid Layout With Zoom Effect</p>
-                    
-                    <div class="tz-gallery">
-
-                        <div class="row">
-                            <div class="col-sm-6 col-md-4">
-                                <a class="lightbox" href="/images/gallery/park.jpg">
-                                    <img src="/images/gallery/park.jpg" alt="Park">
-                                </a>
-                            </div>
-                            <div class="col-sm-6 col-md-4">
-                                <a class="lightbox" href="/images/gallery/bridge.jpg">
-                                    <img src="/images/gallery/bridge.jpg" alt="Bridge">
-                                </a>
-                            </div>
-                            <div class="col-sm-6 col-md-4">
-                                <a class="lightbox" href="/images/gallery/tunnel.jpg">
-                                    <img src="/images/gallery/tunnel.jpg" alt="Tunnel">
-                                </a>
-                            </div>
-                            <div class="col-sm-6 col-md-4">
-                                <a class="lightbox" href="/images/gallery/coast.jpg">
-                                    <img src="/images/gallery/coast.jpg" alt="Coast">
-                                </a>
-                            </div>
-                            <div class="col-sm-6 col-md-4">
-                                <a class="lightbox" href="/images/gallery/rails.jpg">
-                                    <img src="/images/gallery/rails.jpg" alt="Rails">
-                                </a>
-                            </div>
-                            <div class="col-sm-6 col-md-4">
-                                <a class="lightbox" href="/images/gallery/traffic.jpg">
-                                    <img src="/images/gallery/traffic.jpg" alt="Traffic">
-                                </a>
-                            </div>
-                            <div class="col-sm-6 col-md-4">
-                                <a class="lightbox" href="/images/gallery/rocks.jpg">
-                                    <img src="/images/gallery/rocks.jpg" alt="Rocks">
-                                </a>
-                            </div>
-                            <div class="col-sm-6 col-md-4">
-                                <a class="lightbox" href="/images/gallery/benches.jpg">
-                                    <img src="/images/gallery/benches.jpg" alt="Benches">
-                                </a>
-                            </div>
-                            <div class="col-sm-6 col-md-4">
-                                <a class="lightbox" href="/images/gallery/sky.jpg">
-                                    <img src="/images/gallery/sky.jpg" alt="Sky">
-                                </a>
-                            </div>
-                        </div>
-
-                    </div>
-
-                </div>
-            </div> -->
             <div class="gallery-container">
-                <h1><?php echo (($lang == 'ar') ? 'رواق' : (($lang == 'en') ? 'Gallery' : 'Galerie')); ?></h1>
+                <hr class="section-title-line">
+                <h1 class="section-title uppercase"><?php echo (($lang == 'ar') ? 'رواق' : (($lang == 'en') ? 'Gallery' : 'Galerie')); ?></h1>
                 <div class="tz-gallery">
                     <div class="row">
                     <?php for($i=1;$i<=9;$i++){ ?>
@@ -109,7 +33,8 @@
         </div>
         <div class="row">
             <div class="gallery-container">
-                <h1><?php echo (($lang == 'ar') ? 'الإقامة' : (($lang == 'en') ? 'accommodation' : 'Hébérgement')); ?></h1>
+                <hr class="section-title-line">
+                <h1 class="section-title uppercase"><?php echo (($lang == 'ar') ? 'الإقامة' : (($lang == 'en') ? 'accommodation' : 'Hébérgement')); ?></h1>
                 <div class="tz-gallery">
                     <div class="row">
                     <?php for($i=1;$i<=17;$i++){ ?>
@@ -126,7 +51,8 @@
         <div class="row">
             <div class="youtube">
                 <div class="ukraine">
-                    <h2><?php echo (($lang == 'ar') ? 'أوكرانيا' : (($lang == 'en') ? 'Ukraine' : 'Ukraine')); ?></h2>
+                    <hr class="section-title-line">
+                    <h1 class="section-title uppercase"><?php echo (($lang == 'ar') ? 'أوكرانيا' : (($lang == 'en') ? 'Ukraine' : 'Ukraine')); ?></h2>
                     <div class="row">
                         <div class="col-md-4"><iframe src="https://www.youtube.com/embed/XpTAlgdgfDk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
                         <div class="col-md-4"><iframe src="https://www.youtube.com/embed/fBGHdqXuD0E" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
@@ -134,7 +60,8 @@
                     </div>
                 </div>
                 <div class="russia">
-                    <h2><?php echo (($lang == 'ar') ? 'روسيا' : (($lang == 'en') ? 'Russia' : 'Russie')); ?></h2>
+                    <hr class="section-title-line">
+                    <h1 class="section-title uppercase"><?php echo (($lang == 'ar') ? 'روسيا' : (($lang == 'en') ? 'Russia' : 'Russie')); ?></h2>
                     <div class="row">
                         <div class="col-md-4"><iframe src="https://www.youtube.com/embed/rVuAf-0RpTA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
                         <div class="col-md-4"><iframe src="https://www.youtube.com/embed/iChZF9MVB4g" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
